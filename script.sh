67afcaa5a4bab518dfb394686682c50218d4d7ff
marcelloalvear <marcelloalvear@gmail.com>
2024-11-11 13:54:47 -0300

    initial commit - add script.sh

---
+++ script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+mkdir /publico
+mkdir /ven
+mkdir /adm
+mkdir /sec
+
+echo "Diretórios criados"
+
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Grupos de usuários criados"
+
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 777 /publico
+chmod 770 /ven
+chmod 770 /adm
+chmod 770 /sec
+
+echo "Permissões dos diretórios especificadas"
+
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Usuários criados"
+echo "Fim"
